8d51b71ad1661684ef4206c81126e6edcc483b40
Batsinda Keynes benoit <batsindakeynesbenoit@Batsindas-MacBook-Air.local>
2026-04-10 21:34:23 +0200

    Ignore session files
    r

--- database/migration_extend_theme_settings.sql
+++
@@ -1,8 +0,0 @@
-INSERT INTO site_settings (setting_key, setting_value) VALUES
-    ('theme_background_color', '#f7f3ee'),
-    ('theme_surface_color', '#fffdf9'),
-    ('theme_text_color', '#1c1712'),
-    ('theme_font_preset', 'inter'),
-    ('theme_spacing_scale', 'normal')
-ON DUPLICATE KEY UPDATE
-    setting_value = setting_value;
